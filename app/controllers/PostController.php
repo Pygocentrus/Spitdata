1abaedce1c668ab5ccc6b26319bcb060e7f2ce0b
Pygocentrus <pierre.guilhou14@gmail.com>
2014-01-24 19:50:35 +0100

    [UPDATE] adding routes plus archi

--- app/controllers/PostController.php
+++ app/controllers/PostController.php
@@ -3,7 +3,20 @@
 class PostController extends Controller{
 	
 	function index(){
-		$this->f3->set('contentType', 'json');
+		$allowedPostTypes = array('article', 'fbPost', 'tweet');
+		$postType         = htmlentities(trim($this->f3->get('PARAMS.postType')));
+		$postLimit        = htmlentities(trim($this->f3->get('PARAMS.nbPosts')));
+		if(in_array($postType, $allowedPostTypes) && preg_match('#^[1-9][0-9]{0,100}$#', $postLimit)){
+			$posts = new Post($this->db, $postType);
+			$this->f3->set('posts', $posts->all($postLimit));			
+			$this->f3->set('currentPage', 'post');
+			$this->f3->set('contentType', 'json');
+			$this->f3->set('finalView', 'back/post/'.$postType.'.json');
+		}else{
+			$this->f3->set('currentPage', '404');
+			$this->f3->set('contentType', 'html');
+			$this->f3->set('view', 'back/staticPages/404.htm');
+		}
 	}
 
 	function create(){
---
+++ app/models/Post.php
@@ -0,0 +1,36 @@
+<?php
+
+class Post extends DB\SQL\Mapper {
+
+    public function __construct(DB\SQL $db, $postType) {
+        parent::__construct($db, $postType);
+    }
+
+    public function all($limit) {
+        $this->load(array(), array('limit' => $limit));
+        return $this->query;
+    }
+
+    public function add() {
+        $this->copyFrom('POST');
+        $this->save();
+    }
+
+    public function getById($id) {
+        $this->load(array('id=?',$id));
+        $this->copyTo('POST');
+    }
+
+    public function edit($id) {
+        $this->load(array('id=?',$id));
+        $this->copyFrom('POST');
+        $this->update();
+    }
+
+    public function delete($id) {
+        $this->load(array('id=?',$id));
+        $this->erase();
+    }
+}
+
+?>
