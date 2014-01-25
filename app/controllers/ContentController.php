a54c42caac12d53a1d4fc7d187b9acd1dae21def
Pygocentrus <pierre.guilhou14@gmail.com>
2014-01-25 16:17:05 +0100

    [UPDATE] adding views and API update

--- app/controllers/ContentController.php
+++ app/controllers/ContentController.php
@@ -3,7 +3,21 @@
 class ContentController extends Controller{
 	
 	function index(){
-		$this->f3->set('contentType', 'json');
+		$allowedContentTypes = array('location', 'dates', 'item');
+		$postType            = trim(htmlentities($this->f3->get('PARAMS.contentType')));
+		$rawContentLimit     = trim(htmlentities($this->f3->get('PARAMS.nbContents')));
+		$contentLimit        = ($rawContentLimit!=null) ? $rawContentLimit : 1;
+		if(in_array($postType, $allowedContentTypes) && preg_match('#^[1-9][0-9]{0,100}$#', $contentLimit)){
+			$posts = new Content($this->db, $postType);
+			$this->f3->set('contents', $posts->all($contentLimit));	
+			$this->f3->set('currentPage', 'content');
+			$this->f3->set('contentType', 'json');
+			$this->f3->set('finalView', 'back/content/'.$postType.'.json');
+		}else{
+			$this->f3->set('currentPage', '404');
+			$this->f3->set('contentType', 'html');
+			$this->f3->set('view', 'back/staticPages/404.htm');
+		}
 	}
 
 	function create(){
