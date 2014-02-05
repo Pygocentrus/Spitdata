332931c31a3c815f4c61afb92006174d0341cf36
pierreburgy <pierre.burgy@gmail.com>
2014-02-05 17:10:16 +0100

    bug image

--- app/controllers/ImageController.php
+++ app/controllers/ImageController.php
@@ -1,10 +1,23 @@
-<?php 
+<?php
 
 class ImageController extends Controller{
-	
+
 	function index(){
-		$this->f3->set('contentType', 'json');
-	}
+		$image         = new Image();
+		$rawImageLimit = trim(htmlentities($this->f3->get('PARAMS.nbImages')));
+		$userLimit    = ($rawImageLimit!=null) ? $rawImageLimit : 1;
+		if(preg_match('#^[1-9][0-9]*$#', $userLimit))
+            $this->f3->set('limit', $userLimit);
+        else
+        	$this->f3->set('limit', 1);
+        $this->f3->set('images',$image->all($this->f3->get('limit')));
+        // $this->f3->set('images','superimage.jpg');
+        // $this->f3->set('images',$image->all());
+
+		$this->f3->set('contentType', 'html');
+		$this->f3->set('currentPage', 'admin');
+		$this->f3->set('view', 'back/staticPages/image.htm');
+    }
 
 	function create(){
 
@@ -15,7 +28,7 @@ class ImageController extends Controller{
 	}
 
 	function delete(){
-		
+
 	}
 
 }
---
+++ app/models/Image.php
@@ -0,0 +1,37 @@
+<?php
+
+class Image extends Model {
+
+    public function __construct() {
+        parent::__construct();
+        $this->mapper=$this->getMapper(image);
+    }
+
+    public function all($limit) {
+        // return $this->mapper->find(array(), array('limit' => $limit, "order" => 'RAND()'));
+        return 'superimageall.jpg';
+    }
+
+    public function add() {
+        $this->mapper->copyFrom('POST');
+        $this->mapper->save();
+    }
+
+    public function getById($id) {
+        $this->mapper->load(array('id=?',$id));
+        $this->mapper->copyTo('POST');
+    }
+
+    public function edit($id) {
+        $this->mapper->load(array('id=?',$id));
+        $this->mapper->copyFrom('POST');
+        $this->mapper->update();
+    }
+
+    public function delete($id) {
+        $this->mapper->load(array('id=?',$id));
+        $this->mapper->erase();
+    }
+}
+
+?>
