2d4008e73993da3ea15be20e307c6190fe87b4ed
Pygocentrus <pierre.guilhou14@gmail.com>
2014-03-06 22:30:12 +0100

    [UPDATE] Adding The Movie Database API and API front integration

--- app/controllers/ContentController.php
+++ app/controllers/ContentController.php
@@ -25,6 +25,19 @@ class ContentController extends Controller {
         return $contents->all($params['limit']);
 	}
 
+	public function getMovies(){
+		$web = \Web::instance();
+		$url = "https://api.themoviedb.org/3/discover/movie?api_key=55263b0ca3e0979c6508a2b35e765994";
+		$rawYear = trim(htmlentities($this->f3->get('PARAMS.year')));
+		$year = (!empty($rawYear) && preg_match('/^[1-2][0-9]{3}$/', $rawYear)) ? $rawYear : null;
+		if(!is_null($year)){
+			$result = $web->request($url."&year=".$year);
+			print_r($result);
+		}else
+			$this->f3->reroute('/');
+		exit;
+	}
+
 	public function create(){
 
 	}
